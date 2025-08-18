8c8628daeba7471a5bc77b7b9908296faf1ec181
hedi0410 <h.boubecker@gmail.com>
2025-08-18 11:36:24 +0200

    changed prices

--- js/personalAssetsArray.js
+++ js/personalAssetsArray.js
@@ -1,31 +1,31 @@
 export const personalAssets = [
     {
         name: "Flagship Phone Pro",
-        price: 1,
+        price: 2500,
         bonus: 2,
         text: "The Top of the line phone in its Pro version. -2% in all base businesses price."
     },
     {
         name: "Professional Laptop",
-        price: 1,
+        price: 7000,
         bonus: 2,
         text: "A powerful and elegant professional laptop. -2% in all base businesses price."
     },
     {
         name: "Luxury Watch",
-        price: 1,
+        price: 50000,
         bonus: 1,
         text: "A majestic luxury watch, 100% handmade. -1% in all base businesses price."
     },
     {
         name: "Luxury Sport Car",
-        price: 1,
+        price: 370000,
         bonus: 3,
         text: "A spotless dream on wheels. -3% in all base businesses price."
     },
     {
         name: "Private Jet",
-        price: 1,
+        price: 9000000,
         bonus: 5,
         text: "A very fast and comfortable private jet. If you own this, you officialy made it. -5% in all base businesses price."
     }
